bbbc98a3ee27c37febf09b83366361b7489de801
Herbert Lange <herbert.lange@cse.gu.se>
2018-01-26 17:26:46 +0100

    test application that just mirrors and prints the input

--- ajax-backend/Main-CGI.hs
+++ ajax-backend/Main-CGI.hs
@@ -1,24 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.CGI
-import Control.Monad
 import Ajax
-import Muste
-import Muste.Grammar
+import Protocol
+import Database hiding (main)
+
+
 import PGF
 
-cgi:: Grammar -> CGI CGIResult
-cgi grammar =
+import Muste
+import Muste.Grammar
+
+import Database.SQLite.Simple
+
+import Network.SCGI
+import Network.CGI
+import Network
+
+import Data.Map
+
+import Control.Monad
+
+
+
+-- -- Switch loggin on/off
+-- logging = True
+-- logFile = "messagelog.txt"
+
+handleCGI :: Connection -> Map String Grammar -> LessonsPrecomputed -> CGI CGIResult
+handleCGI conn grammars prec =
   do
     setHeader "Content-type" "text/json"
     b <- getBody
-    liftIO $ putStrLn $ "CGI" ++ b
-    result <- liftIO $ handleClientRequest grammar b
-    output result
+    liftIO $ putStrLn (show b)
+    output b
+    
+-- cgi grammar =
+--   do
+
+--     b <- getBody
+--     liftIO $ putStrLn $ "CGI" ++ b
+--     result <- liftIO $ handleClientRequest grammar b
+--     output result
     
 main :: IO ()
 main =
   do
-    pgf <- readPGF "Grammar.pgf"
-    let grammar = pgfToGrammar pgf
-    runCGI (cgi grammar)
+    dbConn <- open "muste.db"
+    (grammars,precs) <- initPrecomputed dbConn
+    runSCGIConcurrent 10 (PortNumber 9000) (handleCGI dbConn grammars precs)
+    return ()
